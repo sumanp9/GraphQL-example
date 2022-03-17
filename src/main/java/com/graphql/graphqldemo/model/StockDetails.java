4eaab8336fbf244bbd89614921dd4ce755158455
sumanp9 <11.sumanpradhan@gmail.com>
2022-03-17 15:28:43 -0500

    GraphQL-demo complete

---
+++ src/main/java/com/graphql/graphqldemo/model/StockDetails.java
@@ -0,0 +1,17 @@
+package model;
+
+import lombok.Data;
+
+@Data
+public class StockDetails {
+    private String ticker;
+    private String stockValue;
+    private String volume;
+    private String averageVolume;
+    private String PE_Ratio;
+    private String EPS;
+    private String dividend;
+    private String marketCap;
+    private String previousOpen;
+    private String previousClose;
+}
